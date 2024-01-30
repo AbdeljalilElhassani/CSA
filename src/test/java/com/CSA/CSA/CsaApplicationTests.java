e0cb9d8b0da070dba9d1b873eeecd5f58fc23d35
Abdeljalil Elhassani <abdeljalilelhassani01@gmail.com>
2024-01-30 10:49:51 +0100

    uploading files V0.1

---
+++ src/test/java/com/CSA/CSA/CsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CSA.CSA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
